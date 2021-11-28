0297eb379210ca7c4953d9453df6adc8e8fb7877
ritikb007 <94009884+ritikb007@users.noreply.github.com>
2021-11-27 20:00:18 -0800

    Co-authored-by: heribertom127 <heribertom127@users.noreply.github.com>

--- client/layouts/dashboard.vue
+++ client/layouts/dashboard.vue
@@ -5,12 +5,11 @@
     style="background-color: #F0F2F5"
     > 
       
-      <v-container 
-      class="py-10 px-6"
-      fluid
-      >
+       
+     
+    
         <Nuxt/>
-      </v-container>
+      
   
     </v-main>
   </v-app>     
--- client/pages/upload.vue
+++ client/pages/upload.vue
@@ -1,9 +1,15 @@
 <template>
+  <div class="grid place-items-center h-screen"
+>
+    <UploadBox/>
+  </div>
  
 </template>
 
 <script>
+import UploadBox from '../components/UploadBox.vue';
 export default {
+  components: { UploadBox },
   layout: "dashboard",
 };
 </script>
