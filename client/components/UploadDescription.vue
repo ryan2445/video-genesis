14194be425befe32295df9ac74968495899bc9df
ritikb007 <94009884+ritikb007@users.noreply.github.com>
2021-11-30 10:47:46 -0800

    Upload page will now have link that will direct user to descrition page

---
+++ client/components/UploadDescription.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="flex flex-col justify-center content-center bg-gray-200 p-10">
+      <p>
+        <strong>Upload</strong>
+      </p>
+       <p>
+        <strong>Details</strong>
+      </p>
+       <p>
+        <strong>Title(Required)</strong>
+      </p>
+      <v-text-field placeholder="Placeholder Text">
+                </v-text-field>
+
+        <p>
+        <strong>Description</strong>
+      </p>
+      <v-text-field placeholder="What is your video about">
+                </v-text-field>
+      <v-container>
+          
+    <v-row>
+      <v-col col="12">
+        <v-combobox
+          v-model="select"
+          :items="items"
+          label="Select Category"
+          multiple
+        ></v-combobox>
+      </v-col>
+    </v-row>
+  </v-container>
+    <p>
+        <strong>Thumbnail</strong>
+      </p>
+  <div>
+      <input type="file" id="upload-file" class="upload-file" />
+    </div>
+    </div>
+  
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      
+      items: ["Music", "Funny", "inspirational", "Sports"],
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ client/pages/Description.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="grid place-items-center h-screen"
+>
+    <UploadDescription/>
+  </div>
+ 
+</template>
+
+<script>
+import UploadDescription from '../components/UploadDescription.vue';
+export default {
+  components: { UploadDescription },
+  layout: "dashboard",
+};
+</script>
+
+<style scoped>
+</style>
