b8a187020ccc338dbc1a56286777f1f255dc44b3
ryan2445 <ryan@snapdesk.app>
2021-12-05 19:08:58 -0800

    Added check-circle icon

--- client/assets/icons/icons.ts
+++ client/assets/icons/icons.ts
@@ -1,13 +1,17 @@
 export type IconsId =
+  | "check-circle"
   | "video";
 
 export type IconsKey =
+  | "CheckCircle"
   | "Video";
 
 export enum Icons {
+  CheckCircle = "check-circle",
   Video = "video",
 }
 
 export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
-  [Icons.Video]: "61697",
+  [Icons.CheckCircle]: "61697",
+  [Icons.Video]: "61698",
 };
