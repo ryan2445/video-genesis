672f45b7f85a0dc244e5b96cbed1967f1d486b54
ryan2445 <ryan@snapdesk.app>
2021-12-05 19:10:47 -0800

    Client-side config + added more video store routes

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -1,14 +1,12 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: process.env.API_URL || 'http://localhost:3000/',
-    timeout: 5000,
-    headers: {
-        
-    }
-});
+	baseURL: process.env.API_URL || 'http://localhost:3001/',
+	timeout: 5000,
+	headers: {}
+})
 
 // This allows us to access axios in any context by calling this.$axios
 export default (_, inject) => {
-    inject('axios', instance)
+	inject('axios', instance)
 }
--- client/store/videos.js
+++ client/store/videos.js
@@ -1,21 +1,53 @@
 export const state = () => ({
-    videos: null
+	videos: null
 })
 
 export const getters = {
-    videos: (state) => state.videos
+	videos: state => state.videos
 }
 
 export const actions = {
-    async videosGet({commit, rootState}) {
-        const response = await this.$axios.get(`videos?username=${rootState.user.user.username}`)
-
-        commit('videosSet', response.data.Items)
-    }
+	async videosGet({ commit, rootState }) {
+		try {
+			const response = await this.$axios.get(
+				`videos?username=${rootState.user.user.username}`
+			)
+			commit('videosSet', response.data.Items)
+		} catch (exception) {
+			return null
+		}
+	},
+	async videosPost({ commit, rootState }, params) {
+		try {
+			const response = await this.$axios.post('videos', {
+				...params,
+				username: rootState.user.user.username
+			})
+			return response.data
+		} catch (exception) {
+			return null
+		}
+	},
+	async videosPut({ commit, rootState }, params) {
+		try {
+			const response = await this.$axios.put('videos', params)
+			return response.data
+		} catch (exception) {
+			return null
+		}
+	},
+	async videosDelete({ commit, rootState }, params) {
+		try {
+			const response = await this.$axios.delete('videos', params)
+			return response.data
+		} catch (exception) {
+			return null
+		}
+	}
 }
 
 export const mutations = {
-    videosSet(state, array) {
-        state.videos = array
-    }
+	videosSet(state, array) {
+		state.videos = array
+	}
 }
