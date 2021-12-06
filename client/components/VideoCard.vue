cbd5d05e220116bc1099e2cfc4f1a1f6e8836316
Manoj166 <mselvakumar@nevada.unr.edu>
2021-12-05 18:06:06 -0800

    videocard

--- client/components/VideoCard.vue
+++ client/components/VideoCard.vue
@@ -1,17 +1,104 @@
 <template>
   <v-container>
-    <v-card max-width="50%">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
-        corrupti. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Numquam, eum. Ab facilis porro ex nam. Nobis a pariatur fuga vero!
-      </p>
-    </v-card>
+<!--
+      <v-card
+      v-for="({title, username, description}) in data" :key="title"
+      class="mx-auto"
+      outlined
+    >
+    <v-list-item three-line>
+      <v-list-item-content>
+          <v-img
+            height="250"
+            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+          ></v-img>
+        <div class="text-overline mb-4">
+          {{title}}
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+          {{username}}
+        </v-list-item-title>
+        <v-list-item-subtitle>{{description}}</v-list-item-subtitle>
+      </v-list-item-content>
+
+    </v-list-item>
+
+
+  </v-card>
+
+  -->
+
+  <br /> <br /> <br /> 
+
+      <div v-for="({title, username, description}) in data" :key="title"> 
+      <v-card
+        class="mx-auto"
+        outlined
+      >
+        <v-row>
+          <v-col cols="4">
+                  
+            <v-img
+              height="250"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+          </v-col>
+          <v-col cols="8">
+            
+
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div class="text-overline mb-4">
+                  {{title}}
+                </div>
+                <v-list-item-title class="text-h5 mb-1">
+                  {{username}}
+                </v-list-item-title>
+                <v-list-item-subtitle>{{description}}</v-list-item-subtitle>
+              </v-list-item-content>
+
+            </v-list-item>
+          </v-col>
+        </v-row>
+
+      </v-card>
+      <br /> 
+    </div>
   </v-container>
 </template>
-
+ 
 <script>
-export default {};
-</script>
+export default {
+  name: "VideoCard",
 
+  props: [],
+
+  data() {
+    return {
+      data: [],
+    };
+  },
+  created() {
+    this.data = 
+    [
+      {
+        title: "fake title", 
+        username: "fake username", 
+        description: "fake discription"
+      }, 
+      {
+        title: "fake title1", 
+        username: "fake username1", 
+        description: "fake discription1"
+      }, 
+      {
+        title: "fake title2222", 
+        username: "fake username2222", 
+        description: "fake discriptio2222n"
+      }
+    ]
+  },
+};
+</script>
+ 
 <style></style>
