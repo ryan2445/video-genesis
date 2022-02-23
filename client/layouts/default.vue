4032d5e10ab2a647658359fdf757a42b4c25ec79
heribertom127 <heribertom1270@gmail.com>
2022-02-22 21:25:55 -0800

    adding the banner to the homepage

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,18 +1,15 @@
 <template>
-    <v-app>
-        <v-app-bar
-            color="white"
-            elevation="0"
-            app
-        >
-            <AppBar />
-        </v-app-bar>
-        <v-main>
-            <v-container>
-                <Nuxt />
-            </v-container>
-        </v-main>
-    </v-app>
+  <v-app>
+    <v-app-bar color="white" elevation="0" app>
+      <AppBar />
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 ##Testing out github
