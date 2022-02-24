193accc4a26114c146890cd60a5efaf0996c25b8
Ryan Hoffman <ryan@snapdesk.app>
2022-02-24 15:52:34 -0800

    Fixed bug in auth middleware causing users to be signed out when navigating to signin, signup, or the home page

--- client/middleware/authMiddleware.js
+++ client/middleware/authMiddleware.js
@@ -1,29 +1,35 @@
 export default async function ({ $axios, $auth, route, store, redirect }) {
-	//  Ignore auth middleware on home, signin, and signup routes
-	if (
-		route.path == '/' ||
-		route.path == '/auth/sign-in' ||
-		route.path == '/auth/sign-up'
-	)
-		return
+  //  Initialize variable to hold the user's session
+  var session = null
 
-	//  Get the cognito user's idtoken
-	const session = await $auth.currentSession()
+  //  Get the cognito user's session (checking if they're logged in)
+  try {
+    session = await $auth.currentSession()
+  } catch (error) {
+    //  Ignore auth middleware on home, signin, and signup routes
+    if (
+      route.path == "/" ||
+      route.path == "/auth/sign-in" ||
+      route.path == "/auth/sign-up" ||
+      route.path == "/auth/confirm" ||
+      route.path == "/auth/reset"
+    )
+      return
 
-	//  If not logged in, redirect to signin
-	if (!session) return redirect('/auth/sign-in')
+    return redirect("/auth/sign-in")
+  }
 
-	//  Get the identity of the user
-	const identity = session.getIdToken()
+  //  Get the identity of the user
+  const identity = session.getIdToken()
 
-	//  Get the user's JWT
-	const token = identity.getJwtToken()
+  //  Get the user's JWT
+  const token = identity.getJwtToken()
 
-	//  Set the Authorization token for all axios requests
-	$axios.defaults.headers.common['Authorization'] = token
+  //  Set the Authorization token for all axios requests
+  $axios.defaults.headers.common["Authorization"] = token
 
-	//  If there is a logged in user, set the user in the store
-	return store.commit('user/setUser', {
-		username: identity.payload['cognito:username']
-	})
+  //  If there is a logged in user, set the user in the store
+  return store.commit("user/setUser", {
+    username: identity.payload["cognito:username"]
+  })
 }
