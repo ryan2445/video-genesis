e55cefd27bfd346d72eb120c9e487a1c9701ed70
Ryan Hoffman <ryan@snapdesk.app>
2022-02-24 15:54:14 -0800

    Password reset feature w.i.p.

--- client/components/ConfirmAuthForm.vue
+++ client/components/ConfirmAuthForm.vue
@@ -1,113 +1,99 @@
 <template>
-    <validation-observer ref="validationObservation" v-slot="{ invalid }">
-        <v-form
-            ref="confirmAuthForm"
-            @submit.prevent="onSubmit"
+  <validation-observer ref="validationObservation" v-slot="{ invalid }">
+    <v-form ref="confirmAuthForm" @submit.prevent="onSubmit">
+      <!-- Code Input -->
+      <validation-provider
+        name="Verification code"
+        rules="required|numeric|length:6"
+        v-slot="{ errors }"
+      >
+        <v-text-field
+          id="confirmAuth"
+          :error="!!errors"
+          :error-messages="errors"
+          placeholder="Code"
+          :success="!errors"
+          v-model="code"
         >
-            <!-- Code Input -->
-            <validation-provider
-                name="username"
-                rules="required|numeric|length:6"
-                v-slot="{ errors }"
-            >
-                <v-text-field
-                    id="confirmAuth"
-                    :error="!!errors"
-                    :error-messages="errors"
-                    placeholder="Code"
-                    :success="!errors"
-                    v-model="code"
-                >
-                </v-text-field>
-            </validation-provider>
+        </v-text-field>
+      </validation-provider>
 
-            <!-- Submit Button -->
-            <div class="flex flex-row">
-                <v-btn
-                    @click="onSubmit"
-                    :disabled="invalid"
-                >
-                    Submit
-                </v-btn>
-                <v-btn
-                    @click="onResend"
-                >
-                    Resend Code
-                </v-btn>
-            </div>
-        </v-form>
-    </validation-observer>
+      <!-- Submit Button -->
+      <div class="text-center">
+        <div class="my-2">
+          <v-btn
+            @click="onSubmit"
+            class="white--text"
+            color="orange lighten-1"
+            :disabled="invalid"
+          >
+            Submit
+          </v-btn>
+        </div>
+        <div class="my-2">
+          <v-btn @click="onResend" small text> Resend Code </v-btn>
+        </div>
+      </div>
+    </v-form>
+  </validation-observer>
 </template>
 
-<script lang="js">
+<script>
 export default {
-    name: "ConfirmAuthForm",
-    props: {
+  data() {
+    return {
+      // The v-model value bounded to the code input
+      code: null,
 
-    },
-    data() {
-        return {
-            // The v-model value bounded to the code input
-            code: null,
-
-            // Flag that determines if the request is loading
-            loading: false
-        }
-    },
-    methods: {
-        async onSubmit() {
-            // If we are loading, return
-            if (this.loading) return
-
-            // Indicate that we are loading
-            this.loading = true
-
-            // Check if the form is valid
-            const isValid = await this.$refs.validationObservation.validate()
-
-            // If the form is not valid, return
-            if (!isValid) {
-                this.loading = false
-                return
-            }
-
-            this.confirmSignUp()
-        },
-
-        async confirmSignUp() {
-            // Send a confirm sign up request
-            try {
-                await this.$auth.confirmSignUp(
-                    this.$store.getters['user/user'].username,
-                    this.code
-                );
-
-                this.$emit('confirmed')
-
-                // Commit it
-                this.$store.dispatch('auth/authorize', {
-                    auth: this.$auth,
-                    axios: this.$axios
-                })
-            }
-            catch (error) {
-                console.log('error confirming sign up', error)
-            }
-        },
-
-        async onResend() {
-            try {
-                await this.$auth.resendSignUp(
-                    this.$store.getters['user/user'].username
-                )
-            }
-            catch (error) {
-                console.log('error resending code:', error)
-            }
-        }
-    },
-    computed: {
-
+      // Flag that determines if the request is loading
+      loading: false,
     }
+  },
+  methods: {
+    async onSubmit() {
+      // If we are loading, return
+      if (this.loading) return
+
+      // Indicate that we are loading
+      this.loading = true
+
+      // Check if the form is valid
+      const isValid = await this.$refs.validationObservation.validate()
+
+      // If the form is not valid, return
+      if (!isValid) {
+        this.loading = false
+        return
+      }
+
+      this.confirmSignUp()
+    },
+    async confirmSignUp() {
+      // Send a confirm sign up request
+      try {
+        await this.$auth.confirmSignUp(
+          this.$store.getters["user/user"].username,
+          this.code
+        )
+
+        this.$emit("confirmed")
+
+        // Commit it
+        this.$store.dispatch("auth/authorize", {
+          auth: this.$auth,
+          axios: this.$axios,
+        })
+      } catch (error) {
+        console.log("error confirming sign up", error)
+      }
+    },
+    async onResend() {
+      try {
+        await this.$auth.resendSignUp(this.$store.getters["user/user"].username)
+      } catch (error) {
+        console.log("error resending code:", error)
+      }
+    },
+  },
 }
 </script>
---
+++ client/pages/auth/reset.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="flex flex-col m-auto text-center" style="width: 360px">
+    <div v-if="!success">
+      <h1>Reset</h1>
+      <validation-provider
+        name="Verification code"
+        rules="required|numeric|length:6"
+        v-slot="{ errors }"
+      >
+        <v-text-field
+          id="resetCode"
+          :error="!!errors"
+          :error-messages="errors"
+          placeholder="Code"
+          :success="!errors"
+          type="number"
+          v-model="code"
+        >
+        </v-text-field>
+      </validation-provider>
+      <validation-provider
+        name="Password"
+        rules="required|min:8"
+        v-slot="{ errors }"
+      >
+        <v-text-field
+          id="passwordAuth"
+          :error-messages="errors"
+          placeholder="Password"
+          :success="!errors"
+          v-model="password"
+          type="password"
+          color="orange lighten-1"
+        />
+      </validation-provider>
+      <v-btn
+        @click="resetPassword()"
+        :disabled="!password || !code"
+        class="mt-3 white--text"
+        color="orange lighten-1"
+      >
+        Submit
+      </v-btn>
+    </div>
+    <div v-else>
+      <h1>You're password has successfully been reset.</h1>
+      <v-btn
+        @click="$router.push('/home')"
+        color="orange lighten-1"
+        text
+        style="text-transform: none"
+      >
+        Click here to login
+      </v-btn>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      username: null,
+      code: null,
+      password: null,
+      success: false
+    }
+  },
+  mounted() {
+    this.username = this.$route.query.username
+
+    this.$auth
+      .forgotPassword(this.username)
+      .then((data) => {
+        console.log("Reset Password Email Sent", data)
+      })
+      .catch((err) => console.log(err))
+  },
+  methods: {
+    resetPassword() {
+      this.$auth
+        .forgotPasswordSubmit(this.username, this.code, this.password)
+        .then((data) => {
+          if (data) this.success = true
+        })
+        .catch((err) => console.log(err))
+    }
+  }
+}
+</script>
