877bde8c76c3f69128c2b46514291b7517141518
n1ddeh <markminkoff00@gmail.com>
2022-03-02 17:02:05 -0800

    cleaning up downsample and upscale

--- api/SuperResolution/utils.py
+++ api/SuperResolution/utils.py
@@ -97,27 +97,30 @@ def downscale_video(video_src):
     
   return None
 
-def downsample_video(video_path = "original.mp4"):
-  if not os.path.isfile(video_path):
-    raise RuntimeError(f"Unknown file path {video_path}")
+def downsample_video(input_path = "original.mp4", output_path = "lr.mp4"):
+  # Ensure the video_path is a valid file
+  if not os.path.isfile(input_path):
+    raise RuntimeError(f"Unknown file path {input_path}")
   
-  output_video = 'lr.mp4'
+  # If the output path is a file, remove it
+  if os.path.isfile(output_path):
+    os.remove(output_path)
   
-  if os.path.isfile(output_video):
-    os.remove(output_video)
-  
-  cap = cv2.VideoCapture(video_path)
+  # Capture the first frame of the video
+  cap = cv2.VideoCapture(input_path)
   success, frame = cap.read()
   
+  # If the first frame cannot be captured, raise a runtime error
   if not success:
     raise RuntimeError("Cannot read video")
   
+  # Set the params for the ffmpeg command
   height = frame.shape[0]
   width = frame.shape[1]
-  scale_factor = 2
-  scaling_algorithm = "bicubic"
+  scale_factor = 4
+  scaling_algorithm = "bicubic" # Chose a different scaling algorithm here: https://ffmpeg.org/ffmpeg-scaler.html
   
-  command = f'ffmpeg -i "{video_path}" -vf scale=ceil({width // scale_factor}/2)*2:ceil({height // scale_factor}/2)*2 -sws_flags {scaling_algorithm} -c:a copy "{output_video}"'
+  command = f'ffmpeg -i "{input_path}" -vf scale=ceil({width // scale_factor}/2)*2:ceil({height // scale_factor}/2)*2 -sws_flags {scaling_algorithm} -c:a copy "{output_path}"'
   subprocess.call(command, shell=True)
 
 def downscale_image(image):
@@ -142,36 +145,49 @@ def downscale_image(image):
   lr_image = tf.cast(lr_image, tf.float32)
   return lr_image
 
-def upscale_video(video_src = "lr.mp4", debug = False):
-  if not os.path.isfile(video_src):
-    raise RuntimeError("Video not found to upscale")
+def upscale_video(input_path = "lr.mp4", output_path = "h4.mp4", debug = False):
+  # Ensure the input_path is a valid file
+  if not os.path.isfile(input_path):
+    raise RuntimeError("Video must be a file to upscale")
 
-  cap = cv2.VideoCapture(video_src)
+  # Init. the video capture stream
+  cap = cv2.VideoCapture(input_path)
   
+  # If the capture is not opened, raise an error
   if (cap.isOpened() == False):
-    raise RuntimeError(f"Could not open {video_src}")
+    raise RuntimeError(f"Could not open {input_path}")
   
+  # Begin reading the video
   success, frame = cap.read()
-  height = frame.shape[0]
-  width = frame.shape[1]
-  fps = cap.get(cv2.CAP_PROP_FPS)
-  frames_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-  output_video_path="hr.mp4"
-  dir = 'temp'
-  path = dir + "/temp.jpeg"
-  model = load_model()
+  
+  # If the first frame cannot be read, raise a runtime error
+  if success is False:
+    raise RuntimeError("Could not read first frame")
 
-  if not os.path.isdir(dir):
-    os.mkdir(dir)
+  # Loads the model
+  model = load_model()
   
-  if os.path.isfile(path):
-    os.remove(path)
-    
-  if os.path.isfile(output_video_path):
-    os.remove(output_video_path)
-    
-  out = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'MP4V'), fps, (width * 4, height * 4))
+  # Removes any old files
+  if os.path.isfile(output_path):
+    os.remove(output_path)
   
+  # Process one frame to get exact height and width of output
+  lr_image = preprocess_image_from_cv(frame)
+  fake_image = model(lr_image)
+  fake_image = postprocess_image(fake_image)
+  
+  # Create the video write stream
+  height = fake_image.shape[0]
+  width = fake_image.shape[1]
+  fps = cap.get(cv2.CAP_PROP_FPS)
+  fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+  out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+  
+  # Count the number of frames for progress loading
+  frames_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+  
+  # Iterate over every frame, predict the super-resolution image, and write to video
+  print("Starting video upscaling...")
   for i in tqdm(range(frames_count - 1)):
     if success is False:
       break
@@ -182,7 +198,11 @@ def upscale_video(video_src = "lr.mp4", debug = False):
     # Generate the image
     fake_image = model(lr_image)
     
+    # Postprocess
     fake_image = postprocess_image(fake_image)
+    
+    # Output to video file
+    out.write(fake_image)
     
     # If debug enabled, show input and output images live
     if debug:
@@ -191,14 +211,20 @@ def upscale_video(video_src = "lr.mp4", debug = False):
       if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
-    out.write(fake_image)
     success, frame = cap.read()
   
   cap.release()
   out.release()
+  
+  print(f"Proccessed {input_path} to higher resolution to {output_path}")
 
 def postprocess_image(image):
-  return tf.cast(tf.clip_by_value(np.asarray(tf.squeeze(image)), 0, 255), tf.uint8).numpy()
+  image = tf.squeeze(image)
+  image = np.asarray(image)
+  image = tf.clip_by_value(image, 0, 255)
+  image = tf.cast(image, tf.uint8).numpy()
+  
+  return image
 
 def download_file(url, path, name):
   # Get the path to output the bytes
@@ -210,6 +236,6 @@ def download_file(url, path, name):
   # Write the contents to the path
   open(full_path, 'wb').write(r.content)
 
-#download_file("https://genesis2vod-staging-output-q1h5l756.s3.us-west-2.amazonaws.com/3LqQwB0qpB8ZyaQYyqYah/3LqQwB0qpB8ZyaQYyqYah_3000.mp4", "", "original.mp4")
-# downsample_video()
-upscale_video(video_src = "lr.mp4", debug = True)
+# download_file("https://genesis2vod-staging-output-q1h5l756.s3.us-west-2.amazonaws.com/3LqQwB0qpB8ZyaQYyqYah/3LqQwB0qpB8ZyaQYyqYah_3000.mp4", "", "original.mp4")
+downsample_video(input_path = "original.mp4", output_path = "lr.mp4")
+upscale_video(input_path = "lr.mp4", output_path= "hr.mp4", debug = True)
