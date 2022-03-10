3ec674046ad0220da3481d1b699aea64bb4a4721
ryan2445 <ryan@snapdesk.app>
2022-03-09 22:16:33 -0800

    refactored users controller

--- api/UsersController/users.py
+++ api/UsersController/users.py
@@ -2,7 +2,11 @@ import boto3
 from boto3.dynamodb.conditions import Key
 import json
 import os
-import uuid
+
+if os.getenv('AWS_SAM_LOCAL'):
+    dynamodb = boto3.resource('dynamodb', endpoint_url = 'http://dynamodb-local:8000').Table('system')
+else:
+    dynamodb = boto3.resource('dynamodb').Table('system')
 
 def badRequest(msg: str) -> dict:
     return {
@@ -10,41 +14,20 @@ def badRequest(msg: str) -> dict:
         'body': msg
     }
 
-if os.getenv('AWS_SAM_LOCAL'):
-    dynamodb = boto3.resource('dynamodb', endpoint_url = 'http://dynamodb-local:8000').Table('system')
-else:
-    dynamodb = boto3.resource('dynamodb').Table('system')
-
-
-
-
 def usersGet(event, context):
-    queryParams = event['queryStringParameters']
     username = event['queryStringParameters']['username']
     response = dynamodb.query(KeyConditionExpression = Key('pk').eq('ID#' + username) & Key('sk').eq("USER"))
-    # else:
-    #     scan_kwargs = {
-    #         'FilterExpression': Key('sk').begins_with('VIDEO')
-    #     }
-    #     response = dynamodb.scan(**scan_kwargs) 
 
     return {
         'statusCode': 200,
         'body': json.dumps(response)
     }
 
-
-
-
-
 def usersPut(event, context):
     body = json.loads(event['body'])
 
     pk = body['pk']
     sk = body['sk']
-    # videoTitle = body['videoTitle']
-    # videoDescription = body['videoDescription']
-    
     optional_keys = [
         'usersFirstName',
         'usersLastName',
@@ -53,21 +36,19 @@ def usersPut(event, context):
         'coverPicKey'
     ]
 
-    keys_with_value = list(filter(lambda x: body.get(x), optional_keys))
+    keys = list(filter(lambda x: body.get(x), optional_keys))
+    if not keys: return badRequest("No attributes to update.")
 
-    if not keys_with_value:
-        raise ValueError("update request should not be empty")
-    update_expr = ", ".join([f"{key}=:{index}" for index, key in enumerate(keys_with_value)])
-
-    expr_attrib_values = dict((f":{index}", body.get(key)) for index, key in enumerate(keys_with_value))
+    update_keys = []
+    update_attributes = []
+    for index, key in enumerate(keys):
+        update_keys.append(f"{key}=:{index}")
+        update_attributes.append((f":{index}", body.get(key)))
     
     response = dynamodb.update_item(
-        Key = {
-            'pk': pk,
-            'sk': sk
-        },
-        UpdateExpression = f"set {update_expr}",
-        ExpressionAttributeValues = expr_attrib_values
+        Key = { 'pk': pk, 'sk': sk },
+        UpdateExpression = f"set {', '.join(update_keys)}",
+        ExpressionAttributeValues = dict(update_attributes)
     )
 
     return {
@@ -75,24 +56,21 @@ def usersPut(event, context):
         'body': json.dumps({ 'response': response })
     }
 
-
 def handle(event, context):
     response = None
-    
+
     methods = {
         'GET': {
             '/users/all': usersGet
         },
-        
         'PUT': {
             '/users': usersPut
         }
-    
     }
-    
+
     method = event['httpMethod']
     path = event['path']
-    
+
     response = methods[method][path](event, context)
 
     if not response:
@@ -102,7 +80,8 @@ def handle(event, context):
         }
 
     response['headers'] = {
-        'Access-Control-Allow-Origin': '*'        
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
     }
 
     return response
