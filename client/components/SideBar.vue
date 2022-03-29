bd4ba5b629563218ca221c452fe8ee98c0753369
ryan2445 <ryan@snapdesk.app>
2022-03-29 15:23:31 -0700

    stylin' side bar / top bar

--- client/components/SideBar.vue
+++ client/components/SideBar.vue
@@ -1,84 +1,63 @@
 <template>
-  <div>
-    <v-navigation-drawer v-model="drawer" mini-variant app permanent>
-      <template v-slot:prepend>
-        <v-list-item class="px-2 orange lighten-1">
-          <v-list-item-avatar>
-            <NuxtLink to="/">
-              <img alt="Avatar" :src="logo" />
-            </NuxtLink>
-          </v-list-item-avatar>
-          <div class="text-white font-weight-bold">Video Genesis</div>
-        </v-list-item>
-      </template>
-      <v-divider></v-divider>
-      <v-list nav>
-        <v-list-item-group color="orange lighten-1">
-          <v-list-item
-            v-for="(item, i) in items"
-            :key="i"
-            :to="item.route"
-            link
-          >
-            <v-tooltip right transition="slide-x-transition" :open-delay="500">
-              <template v-slot:activator=" {on, attrs} ">
-                <v-list-item-icon v-bind="attrs" v-on="on">
-                  <v-icon>{{ item.icon }}</v-icon>
-                </v-list-item-icon>
-              </template>
-              <span>{{ item.title }}</span>
-            </v-tooltip>
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-            
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-      <!-- <template v-slot:append>
+    <v-navigation-drawer app clipped :mini-variant="drawer">
         <v-divider></v-divider>
-        <v-btn class="mx-2" icon @click.stop="drawer = !drawer">
-          <v-icon class="mx-10 my-2">mdi-menu</v-icon>
-        </v-btn>
-      </template> -->
+        <v-list nav>
+            <v-list-item-group color="orange lighten-1">
+                <v-list-item v-for="(item, i) in items" :key="i" :to="item.route" link>
+                    <v-tooltip right :open-delay="250">
+                        <template v-slot:activator=" {on, attrs} ">
+                            <v-list-item-icon v-bind="attrs" v-on="on">
+                                <v-icon>{{ item.icon }}</v-icon>
+                            </v-list-item-icon>
+                        </template>
+                        <span>{{ item.title }}</span>
+                    </v-tooltip>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-item-group>
+        </v-list>
+        <template v-slot:append>
+            <v-btn class="mx-2" icon @click.stop="drawer = !drawer">
+                <v-icon class="mx-10 my-2">mdi-menu</v-icon>
+            </v-btn>
+        </template>
     </v-navigation-drawer>
-  </div>
 </template>
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters } from 'vuex'
 export default {
-  data: () => ({
-    items: [
-      {
-        title: "Home",
-        icon: "mdi-home",
-        route: "/home",
-      },
-      {
-        title: "Upload",
-        icon: "mdi-upload",
-        route: "/upload",
-      },
+    data: () => ({
+        items: [
+            {
+                title: 'Home',
+                icon: 'mdi-home',
+                route: '/home'
+            },
+            {
+                title: 'Upload',
+                icon: 'mdi-upload',
+                route: '/upload'
+            },
 
-      {
-        title: "Explore",
-        icon: "mdi-telescope",
-        route: "/explore",
-      },
-      {
-        title: "Contact Us",
-        icon: "mdi-account-question ",
-        route: "/contactus",
-      },
-    ],
-    drawer: true,
-    logo: "https://upload.wikimedia.org/wikipedia/commons/1/1a/V_logo.png",
-  }),
-  computed: {
-    ...mapGetters({
-      user: "user/user",
+            {
+                title: 'Explore',
+                icon: 'mdi-telescope',
+                route: '/explore'
+            },
+            {
+                title: 'Contact Us',
+                icon: 'mdi-account-question ',
+                route: '/contactus'
+            }
+        ],
+        drawer: true
     }),
-  },
-};
+    computed: {
+        ...mapGetters({
+            user: 'user/user'
+        })
+    }
+}
 </script>
-<style scoped></style>
--- client/components/TopBar.vue
+++ client/components/TopBar.vue
@@ -1,128 +1,107 @@
 <template>
-  <v-app-bar app color="orange" :elevation="1" hide-on-scroll style="height:56px;">
-    <div>
-      <h2 class="text-gray-800 text-lg strong">
-        {{ title }}
-      </h2>
-    </div>
-    <v-spacer />
-    <div
-      class="mx-6"
-    >
-      <v-tooltip>
-        <template v-slot:activator="{ on, attrs }">
-          <v-icon
-            v-bind="attrs"
-            v-on="on"
-          >
-            {{ srRunning ? 'icon-wifi-check' : 'icon-wifi-cancel'}}
-          </v-icon>
-        </template>
-        <span>
-          Super Resolution server is {{ srRunning ? '' : 'not'}} running!
-        </span>
-      </v-tooltip>
-    </div>
-    <v-menu offset-y min-width="200px">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          v-bind="attrs"
-          v-on="on"
-          fab
-          depressed
-          color="grey lighten-1"
-          width="45"
-          height="45"
-          class="mb-2 shadow-md"
-        >
-      <!-- <v-icon> mdi-account </v-icon> -->
-        <div
-            v-if="userProfilePic"
-            class="w-14 h-14 rounded-full overflow-hidden mb-2"
-        >
-          <img
-              class="min-w-full min-h-full object-cover"
-              :src="userProfilePic"
-              alt="avatar"
-          />
+    <v-app-bar app clipped-left color="orange" elevation="1" hide-on-scroll>
+        <div class="d-flex align-center">
+            <NuxtLink to="/" class="mr-4">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/1/1a/V_logo.png"
+                    class="rounded-full object-cover" style="width:45px; height:45px;" />
+            </NuxtLink>
+            <div class="text-white font-weight-bold">
+                Video Genesis - {{ title }}
+            </div>
         </div>
-          
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item-group color="orange">
-          <v-list-item
-            v-for="(item, i) in topBarItems"
-            :key="i"
-            @click="handleClick(item)"
-          >
-            <v-list-item-icon class="listItemIcon">
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>
-                {{ item.title }}
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-menu>
-  </v-app-bar>
+        <v-spacer />
+        <div class="mx-6">
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-icon v-bind="attrs" v-on="on">
+                        {{ srRunning ? 'icon-wifi-check' : 'icon-wifi-cancel'}}
+                    </v-icon>
+                </template>
+                <span>
+                    Super Resolution server is {{ srRunning ? '' : 'not'}} running!
+                </span>
+            </v-tooltip>
+        </div>
+        <v-menu offset-y min-width="200px">
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn v-bind="attrs" v-on="on" fab depressed color="grey lighten-1"
+                    width="45" height="45">
+                    <img v-if="userProfilePic" style="height:45px; width:45px;"
+                        class="rounded-full object-cover" :src="userProfilePic" />
+                    <v-icon v-else> mdi-account </v-icon>
+                </v-btn>
+            </template>
+            <v-list>
+                <v-list-item-group color="orange">
+                    <v-list-item v-for="(item, i) in topBarItems" :key="i"
+                        @click="handleClick(item)">
+                        <v-list-item-icon class="listItemIcon">
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                {{ item.title }}
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-menu>
+    </v-app-bar>
 </template>
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters } from 'vuex'
 export default {
-  data() {
-    return {
-      topBarItems: [
-        {
-          title: "Account Settings",
-          color: "orange",
-          icon: "mdi-account",
-          route: "/account",
-        },
-        {
-          title: "Sign Out",
-          icon: "mdi-logout-variant",
-          method: "signOut",
-        },
-      ],
-    };
-  },
-  methods: {
-    handleClick(item) {
-      if (item.route) return this.$router.push(item.route);
+    data() {
+        return {
+            topBarItems: [
+                {
+                    title: 'Account Settings',
+                    color: 'orange',
+                    icon: 'mdi-account',
+                    route: '/account'
+                },
+                {
+                    title: 'Sign Out',
+                    icon: 'mdi-logout-variant',
+                    method: 'signOut'
+                }
+            ]
+        }
+    },
+    methods: {
+        handleClick(item) {
+            if (item.route) return this.$router.push(item.route)
 
-      if (item.method) return this[item.method]();
+            if (item.method) return this[item.method]()
+        },
+        async signOut() {
+            await this.$auth.signOut()
+            this.$store.commit('user/setUser', null)
+            this.$store.dispatch('auth/unauthorize', { axios: this.$axios })
+            this.$router.push('/')
+        }
     },
-    async signOut() {
-      await this.$auth.signOut();
-      this.$store.commit("user/setUser", null);
-      this.$store.dispatch("auth/unauthorize", { axios: this.$axios });
-      this.$router.push("/");
-    },
-  },
-  computed: {
-    title() {
-      return this.$store.getters["app/route"];
-    },
-    ...mapGetters({
-      user: "users/rootUser",
-      srRunning: 'sr/running'
-    }),
-    userProfilePic() {
-      // If the user does not exist, return null
-      if (!this.user) return null;
+    computed: {
+        title() {
+            return this.$store.getters['app/route']
+        },
+        ...mapGetters({
+            user: 'users/rootUser',
+            srRunning: 'sr/running'
+        }),
+        userProfilePic() {
+            // If the user does not exist, return null
+            if (!this.user) return null
 
-      return this.user.profilePicKey || "https://www.unr.edu/main/images/divisions/president/marketing-communications/brand/brand-athletics-2.png";
-    },
-  },
-};
+            return this.user.profilePicKey
+        }
+    }
+}
 </script>
 <style scoped>
 .v-application--is-ltr .v-list-item__action:first-child,
 .v-application--is-ltr .v-list-item__icon:first-child {
-  margin-right: 16px;
+    margin-right: 16px;
 }
 </style>
