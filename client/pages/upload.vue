50351fecdb999cd449c66ffb6c03be224a455c73
ryan2445 <ryan@snapdesk.app>
2022-03-29 15:23:23 -0700

    stylin'

--- client/pages/upload.vue
+++ client/pages/upload.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="grid place-items-center h-screen">
-    <video-form />
-  </div>
+    <div class="d-flex justify-center">
+        <video-form />
+    </div>
 </template>
 
 <script>
-import UploadBox from "../components/UploadBox.vue";
+import UploadBox from '../components/UploadBox.vue'
 export default {
-  components: { UploadBox },
-  layout: "dashboard",
-  created() {
-    this.$store.commit("app/setRoute", "Video Upload");
-  },
-};
+    components: { UploadBox },
+    layout: 'dashboard',
+    created() {
+        this.$store.commit('app/setRoute', 'Video Upload')
+    }
+}
 </script>
 
 <style scoped></style>
