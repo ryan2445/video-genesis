a013ad695627ed80860df25d266a738b87301024
ritikb007 <94009884+ritikb007@users.noreply.github.com>
2022-03-30 20:59:27 -0700

    clean up...

--- client/components/PlaylistVideos.vue
+++ client/components/PlaylistVideos.vue
@@ -31,40 +31,7 @@ export default {
     }),
   },
   async mounted() {
-    this.videos = this.playlist["videos"]['Items'];
-    console.log("here<<<", this.videos, this.videos2);
-    // await this.$store.dispatch("videos/getAllVideos");
-    // var userVideos = await this.$store.dispatch("videos/videosGetByUsername", {
-    //   username: this.user.username,
-
-    // console.log("explore videos");
-    // console.log(this.videos);
-    // console.log("user videos");
-    // console.log(userVideos);
-    // console.log("end");
-
-    // const videoSks = String(this.playlist.videos).split(",");
-    // console.log(videoSks);
-
-    // let videoCardsVideo = [];
-
-    // // view videos from other users
-    // this.videos.forEach((video) => {
-    //   if (videoSks.includes(video.sk)) {
-    //     videoCardsVideo.push(video);
-    //     alert("pushed a video");
-    //   }
-    // });
-
-    // // view users own videos
-    // userVideos.forEach((video) => {
-    //   if (videoSks.includes(video.sk)) {
-    //     videoCardsVideo.push(video);
-    //     alert("pushed a video");
-    //   }
-    // });
-
-    //   this.videoCardsVideo = videoCardsVideo;
+    this.videos = this.playlist["videos"]["Items"];
   },
 };
 </script>
