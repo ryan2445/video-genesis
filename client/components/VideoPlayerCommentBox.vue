487853ee73f68f062f5f03c27f14f2f9d10087ef
heribertom127 <heribertom1270@gmail.com>
2022-04-04 17:59:55 -0700

    more tiny iterations to the comments

--- client/components/VideoPlayerCommentBox.vue
+++ client/components/VideoPlayerCommentBox.vue
@@ -1,5 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <div class="h-20 flex mb-4 mt-2">
+      <div class="p-2 mr-2">
+        <img
+          class="h-full w-full object-cover rounded-full"
+          height="40"
+          width="40"
+          :src="user.profilePicKey"
+          alt="avatar"
+          style="border: solid 1px #e1e1e1"
+        />
+      </div>
+      <!-- main -->
+      <div class="flex flex-col">
+        <!-- header -->
+        <div>
+          <h2 class="mt-3 font-semibold">{{ user.username }}</h2>
+        </div>
+        <div>{{ comment.content }}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
