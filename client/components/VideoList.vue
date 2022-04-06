1a303513d72260c1d4eb68c49930d0257a7fd911
ritikb007 <94009884+ritikb007@users.noreply.github.com>
2022-04-06 03:04:42 -0700

    all playlists

--- client/components/VideoList.vue
+++ client/components/VideoList.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-2 gap-y-3">
+  <div
+    class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-2 gap-y-3"
+  >
     <video-card
       v-for="(video, i) in videos"
       :key="i"
       :video="video"
       :idx="i"
+      :playlists="playlists"
     />
   </div>
 </template>
@@ -15,9 +18,13 @@ export default {
   props: {
     videos: {
       type: Array,
-      required: true
-    }
-  }
+      required: true,
+    },
+    playlists: {
+      type: Array,
+      required: true,
+    },
+  },
 };
 </script>
 
