02d0250aa4b12bbf6ad04ec3b82f7d0b067b3164
ryan2445 <rhoffman@copia.io>
2022-04-05 23:09:57 -0700

    setting app route on user profile page

--- client/pages/users/_id.vue
+++ client/pages/users/_id.vue
@@ -1,60 +1,58 @@
 <template>
-  <user-profile v-if="user" :user="user" />
+    <user-profile v-if="user" :user="user" />
 </template>
 
 <script>
 export default {
-  name: "UserProfilePage",
-  layout: "dashboard",
-  data() {
-    return { 
-      tabSelected: "Uploads", 
-      loading: true, 
-      username: "", 
-      user: undefined,
-      videos: [] 
-    };
-  },
-  methods: {
-    changeTabSelection(tabName) {
-      this.tabSelected = tabName;
+    name: 'UserProfilePage',
+    layout: 'dashboard',
+    data() {
+        return {
+            tabSelected: 'Uploads',
+            loading: true,
+            username: '',
+            user: undefined,
+            videos: []
+        }
     },
-    getQueryParamsAndSetKeys() {
-      const path = this.$route.fullPath.replace('/users/', '');
-      const params = new URLSearchParams(path);
+    methods: {
+        changeTabSelection(tabName) {
+            this.tabSelected = tabName
+        },
+        getQueryParamsAndSetKeys() {
+            const path = this.$route.fullPath.replace('/users/', '')
+            const params = new URLSearchParams(path)
 
-      // If the username is not provided, error
-      if (!params.has('username')) {
-        return;
-      }
+            // If the username is not provided, error
+            if (!params.has('username')) {
+                return
+            }
 
-      const username = params.get('username');
-      this.username = username;
+            const username = params.get('username')
+            this.username = username
+        }
     },
-  },
-  created() {
-    this.$store.commit("app/setRoute", "");
-  },
-  async mounted() {
-    this.getQueryParamsAndSetKeys()
-    const user = await this.$store.dispatch("users/userGetByUsername", this.username);
-    
-    this.user = user;
+    async mounted() {
+        this.getQueryParamsAndSetKeys()
+        const user = await this.$store.dispatch('users/userGetByUsername', this.username)
 
-    //  Stop loading
-    this.loading = false;
-  },
-};
+        this.user = user
+
+        this.$store.commit('app/setRoute', user.username)
+
+        this.loading = false
+    }
+}
 </script>
 
 <style scoped>
 .v-enter-active,
 .v-leave-active {
-  transition: opacity 0.5s ease;
+    transition: opacity 0.5s ease;
 }
 
 .v-enter-from,
 .v-leave-to {
-  opacity: 0;
+    opacity: 0;
 }
 </style>
