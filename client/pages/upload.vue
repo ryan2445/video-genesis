95d26d977a89e3226e6e6782e4aeea92f604ea7b
heribertom127 <heribertom1270@gmail.com>
2022-04-08 16:02:53 -0700

    centered the upload box

--- client/pages/upload.vue
+++ client/pages/upload.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="grid grid-col-1 place-content-center h-screen overflow-y-hidden">
-    <video-form />
+  <div class="overflow-y-hidden">
+    <div class="grid grid-col-1 place-content-center center-upload">
+      <video-form />
+    </div>
   </div>
 </template>
 <script>
@@ -11,4 +13,8 @@ export default {
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.center-upload {
+  height: calc(100vh - 90px);
+}
+</style>
