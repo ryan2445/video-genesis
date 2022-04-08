15a5352d02c01a35547feab531f06abf9fdf31eb
n1ddeh <markminkoff00@gmail.com>
2022-04-08 16:24:17 -0700

    app bar fix

--- client/components/AppBar.vue
+++ client/components/AppBar.vue
@@ -1,33 +1,31 @@
 <template>
-  <div>
-    <v-row justify="space-between" align="center" no-gutters>
-      <v-col cols="4">
-        <NuxtLink :to="$route.path == '/' ? '/home' : '/'">
-          <h1 class="font-medium text-xl mx-4">Video Genesis</h1>
-        </NuxtLink>
-      </v-col>
-      <v-col align="end" cols="8">
-        <div v-if="!user">
-          <v-btn color="#FF7A45" outlined class="m-2" @click="onSignUp">
-            Sign Up
-          </v-btn>
-          <v-btn class="white--text" color="orange accent-3" @click="onSignIn">
-            Sign In
-          </v-btn>
+  <v-row justify="space-between" align="center" no-gutters>
+    <v-col cols="4">
+      <NuxtLink :to="$route.path == '/' ? '/home' : '/'">
+        <h1 class="font-medium text-xl mx-4">Video Genesis</h1>
+      </NuxtLink>
+    </v-col>
+    <v-col align="end" cols="8">
+      <div v-if="!user">
+        <v-btn color="#FF7A45" outlined class="m-2" @click="onSignUp">
+          Sign Up
+        </v-btn>
+        <v-btn class="white--text" color="orange accent-3" @click="onSignIn">
+          Sign In
+        </v-btn>
+      </div>
+      <div
+        v-else
+        class="flex flex-row items-center"
+        style="justify-content: end"
+      >
+        <div class="mx-3" v-if="user">
+          <h1>Logged in as {{ user.username }}</h1>
         </div>
-        <div
-          v-else
-          class="flex flex-row items-center"
-          style="justify-content: end"
-        >
-          <div class="mx-3" v-if="user">
-            <h1>Logged in as {{ user.username }}</h1>
-          </div>
-          <v-btn v-if="user" @click="onSignOut"> Sign Out </v-btn>
-        </div>
-      </v-col>
-    </v-row>
-  </div>
+        <v-btn v-if="user" @click="onSignOut"> Sign Out </v-btn>
+      </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
