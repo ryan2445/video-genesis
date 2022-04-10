480aa9f6e1bcd43daee8db247326a3fa4076dd35
heribertom127 <heribertom1270@gmail.com>
2022-04-09 17:09:08 -0700

    changed ui

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,15 +1,13 @@
 <template>
-    <v-app>
-        <v-app-bar elevation="0" app>
-            <AppBar />
-        </v-app-bar>
-        <v-main>
-            <div>
-                <Nuxt />
-            </div>
-        </v-main>
-    </v-app>
+  <v-app>
+    <v-app-bar elevation="0" app>
+      <AppBar />
+    </v-app-bar>
+    <v-main>
+      <Nuxt />
+    </v-main>
+  </v-app>
 </template>
 <script>
-export default {}
+export default {};
 </script>
