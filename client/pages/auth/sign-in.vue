54d8d71da2bf7676355e066c4957b00e50e53a0c
ritikb007 <94009884+ritikb007@users.noreply.github.com>
2022-04-10 16:03:47 -0700

    components cleanup

--- client/pages/auth/sign-in.vue
+++ client/pages/auth/sign-in.vue
@@ -2,31 +2,6 @@
   <div class="flex flex-col contents-center">
     <div class="flex flex-row justify-left items-center">
       <div class="w-3/4 fix-height bg-gray-500">
-        <!-- <svg
-          width="999"
-          height="800"
-          viewBox="0 0 1230 1024"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-        >
-          <rect width="2000" height="1036" fill="#FFF2E8" />
-          <rect width="2000" height="1036" fill="url(#pattern0)" />
-          <defs>
-            <pattern
-              id="pattern0"
-              patternContentUnits="objectBoundingBox"
-              width="1"
-              height="1"
-            >
-              <use
-                xlink:href="#image0_29_21383"
-                transform="translate(-0.22549) scale(0.00070028)"
-              />
-            </pattern>
-           
-          </defs>
-        </svg> -->
         <img
           id="image0_29_21383"
           class="object-cover object-bottom bg-gray-700 w-full h-full"
--- client/pages/auth/sign-up.vue
+++ client/pages/auth/sign-up.vue
@@ -2,36 +2,6 @@
   <div class="flex flex-col contents-center">
     <div class="flex flex-row justify-left items-center">
       <div class="w-3/4 fix-height bg-gray-500">
-        <!-- <svg
-        width="952"
-        height="800"
-        viewBox="0 0 952 1024"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-      >
-        <rect width="1000" height="1046" fill="#FFF2E8" />
-        <rect width="1000" height="1046" fill="url(#pattern0)" />
-        <defs>
-          <pattern
-            id="pattern0"
-            patternContentUnits="objectBoundingBox"
-            width="1"
-            height="1"
-          >
-            <use
-              xlink:href="#image0_29_11522"
-              transform="translate(-0.323836) scale(0.000872708)"
-            />
-          </pattern>
-          <image
-            id="image0_29_11522"
-            width="1888"
-            height="1259"
-            xlink:href="https://images.unsplash.com/photo-1599551528722-6b6d968512a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-          />
-        </defs>
-      </svg> -->
         <img
           id="image0_29_21383"
           class="object-cover object-bottom bg-gray-700 w-full h-full"
