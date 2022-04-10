52b3cdb11ffa52f9dd0d98462c1544d224fd2bd2
ryan2445 <rhoffman@copia.io>
2022-04-09 19:16:25 -0700

    janky client-side video search

---
+++ client/components/ExploreTop.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="d-flex ma-4">
+        <div class="" style="width:300px;">
+            <v-text-field v-model="searchText" @input="search"
+                prepend-inner-icon="mdi-magnify" placeholder="Search Videos..." rounded
+                outlined dense hide-details color="orange" />
+        </div>
+    </div>
+</template>
+<script>
+import { debounce } from 'lodash'
+export default {
+    data() {
+        return {
+            searchText: null
+        }
+    },
+    methods: {
+        search: debounce(function () {
+            this.$emit('search', this.searchText)
+        }, 500)
+    }
+}
+</script>
--- client/pages/Explore.vue
+++ client/pages/Explore.vue
@@ -1,91 +1,98 @@
 <template>
-  <div>
-    <div v-if="loading" class="text-center" style="height: 100vh">
-      <v-progress-circular indeterminate color="orange" style="top: 50%" />
-    </div>
-    <div
-      v-if="!loading && videos && !videos.length"
-      class="text-center"
-      style="
+    <div>
+        <div v-if="loading" class="text-center" style="height: 100vh">
+            <v-progress-circular indeterminate color="orange" style="top: 50%" />
+        </div>
+        <div v-if="!loading && videos && !videos.length" class="text-center" style="
         display: flex;
         height: 50vh;
         align-items: center;
         justify-content: center;
-      "
-    >
-      <div>No videos found</div>
+      ">
+            <div>No videos found</div>
+        </div>
+        <explore-top @search="searchText = $event, paginate()" />
+        <video-list :videos="videosOnCurrentPage || []" />
+        <div class="text-xs-center">
+            <v-pagination color="orange" v-model="page" :length="paginationLength"
+                @input="paginate"></v-pagination>
+        </div>
     </div>
-    <video-list :videos="videosOnCurrentPage || []" />
-    <div class="text-xs-center">
-      <v-pagination
-        color="orange"
-        v-model="page"
-        :length="paginationLength"
-        @input="paginate"
-      ></v-pagination>
-    </div>
-  </div>
-
-  <!-- <VideoList /> -->
+    <!-- <VideoList /> -->
 </template>
-
 <script>
-import { mapGetters } from "vuex";
-import VideoCard from "../components/VideoCard.vue";
+import { mapGetters } from 'vuex'
+import VideoCard from '../components/VideoCard.vue'
+import { get } from 'lodash'
 export default {
-  components: { VideoCard },
-  layout: "dashboard",
-  name: "Explore",
-  data() {
-    return {
-      //  Controls v-progress-circular
-      loading: true,
-      page: 1,
-      videosOnCurrentPage: null,
-      videosPerPage: 8,
-      paginationLength: null,
-    };
-  },
-  computed: {
-    ...mapGetters({
-      videos: "videos/videos",
-    }),
-  },
-  methods: {
-    async paginate(pageNumber) {
-      let startingIndex = 0;
-      let lastIndex = 0;
-      startingIndex = (pageNumber - 1) * this.videosPerPage;
-      lastIndex = startingIndex + this.videosPerPage;
-
-      if (this.videos.length < lastIndex) {
-        this.videosOnCurrentPage = this.videos.slice(
-          startingIndex,
-          this.videos.length
-        );
-      } else {
-        this.videosOnCurrentPage = this.videos.slice(startingIndex, lastIndex);
-      }
+    components: { VideoCard },
+    layout: 'dashboard',
+    name: 'Explore',
+    data() {
+        return {
+            //  Controls v-progress-circular
+            loading: true,
+            page: 1,
+            videosOnCurrentPage: null,
+            videosPerPage: 8,
+            paginationLength: null,
+            searchText: null
+        }
     },
-  },
+    created() {
+        this.$store.commit('app/setRoute', 'Explore')
+    },
+    async mounted() {
+        //  Send request to get videos
+        await this.$store.dispatch('videos/getAllVideos')
 
-  created() {
-    this.$store.commit("app/setRoute", "Explore");
-  },
-  async mounted() {
-    //  Send request to get videos
-    await this.$store.dispatch("videos/getAllVideos");
+        //  Stop loading
+        this.loading = false
+        this.paginationLength = Math.ceil(this.videos.length / this.videosPerPage)
+        if (this.videos.length > this.videosPerPage) {
+            this.videosOnCurrentPage = this.videos.slice(0, this.videosPerPage)
+        } else {
+            this.videosOnCurrentPage = this.videos.slice(0, this.videos.length)
+        }
+    },
+    computed: {
+        ...mapGetters({
+            videos: 'videos/videos'
+        })
+    },
+    methods: {
+        async paginate(pageNumber = 1) {
+            let videos = this.videos
+            let startingIndex = 0
+            let lastIndex = 0
+            startingIndex = (pageNumber - 1) * this.videosPerPage
+            lastIndex = startingIndex + this.videosPerPage
 
-    //  Stop loading
-    this.loading = false;
-    this.paginationLength = Math.ceil(this.videos.length / this.videosPerPage);
-    if (this.videos.length > this.videosPerPage) {
-      this.videosOnCurrentPage = this.videos.slice(0, this.videosPerPage);
-    } else {
-      this.videosOnCurrentPage = this.videos.slice(0, this.videos.length);
+            if (this.searchText) videos = videos.filter(this.matchesSearchText)
+
+            if (this.videos.length < lastIndex) lastIndex = videos.length
+
+            this.videosOnCurrentPage = videos.slice(startingIndex, lastIndex)
+        },
+        matchesSearchText(video) {
+            const searchText = this.searchText.toLowerCase()
+
+            return (
+                video.videoTitle.toLowerCase().includes(searchText) ||
+                (video.videoDescription &&
+                    video.videoDescription.toLowerCase().includes(searchText)) ||
+                (video.user &&
+                    (video.user.username.toLowerCase().includes(searchText) ||
+                        (video.user.usersFirstName &&
+                            video.user.usersFirstName
+                                .toLowerCase()
+                                .includes(searchText)) ||
+                        (video.user.usersLastName &&
+                            video.user.usersLastName.toLowerCase().includes(searchText))))
+            )
+        }
     }
-  },
-};
+}
 </script>
 
 <style scoped></style>
