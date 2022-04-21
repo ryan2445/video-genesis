ecbbc2d1eb51ef83df20851213d5e947ce799e4f
Manoj166 <mselvakumar@nevada.unr.edu>
2022-04-20 17:59:01 -0700

    footer component

---
+++ client/components/LandingPage/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="footer">
+        <v-row class="w-11/12 mx-auto white--text">
+            <v-col cols="6" class="mt-11 mb-11">
+                <h6>Video Genesis: Super-Resolution<br>
+                    For Everyone
+                </h6>
+            </v-col>
+            <v-col cols="6" class="mt-11 mb-11">
+                <h6 class="float-right">Application Created With Love by Team 24</h6>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<style scoped>
+    .footer {
+        background: #A13440;
+    }
+</style>
