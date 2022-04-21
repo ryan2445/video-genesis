4acf86e82fbe679f15935820bfaa4b9d439651c0
Manoj166 <mselvakumar@nevada.unr.edu>
2022-04-20 17:55:01 -0700

    commented out app bar

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
-    <v-app-bar elevation="0" app>
+   <!--  <v-app-bar elevation="0" app>
       <AppBar />
-    </v-app-bar>
+    </v-app-bar> -->
     <v-main>
       <Nuxt />
     </v-main>
