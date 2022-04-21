74977cda4b9d0b2721446e5afce7939afc6d9b4e
ritikb007 <94009884+ritikb007@users.noreply.github.com>
2022-04-20 18:26:33 -0700

    cleaning up the index

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -10,12 +10,12 @@
 </template>
 
 <script>
-import Banner from "../components/LandingPage/Banner.vue"
-import Team from "../components/LandingPage/Team.vue"
-import ProjectDemo from "../components/LandingPage/ProjectDemo.vue"
-import AppFeatures from "../components/LandingPage/AppFeatures.vue"
-import Resources from "../components/LandingPage/Resources.vue"
-import Footer from "../components/LandingPage/Footer.vue"
+import Banner from "../components/LandingPage/Banner.vue";
+import Team from "../components/LandingPage/Team.vue";
+import ProjectDemo from "../components/LandingPage/ProjectDemo.vue";
+import AppFeatures from "../components/LandingPage/AppFeatures.vue";
+import Resources from "../components/LandingPage/Resources.vue";
+import Footer from "../components/LandingPage/Footer.vue";
 import { mapGetters } from "vuex";
 
 export default {
@@ -25,7 +25,7 @@ export default {
     ProjectDemo,
     AppFeatures,
     Resources,
-    Footer
+    Footer,
   },
   computed: {
     ...mapGetters({
@@ -34,49 +34,7 @@ export default {
   },
   name: "landingpage",
   data() {
-    return {
-      team: [
-        {
-          name: "Mark Minkoff",
-          role: "Web developer",
-          avatar: "",
-        },
-        {
-          name: "Ryan Hoffman",
-          role: "Web developer",
-          avatar: "",
-        },
-        {
-          name: "Ritik Bhardwaj",
-          role: "Web developer",
-          avatar: "",
-        },
-        {
-          name: "Heriberto Martinez",
-          role: "Web developer",
-          avatar: "",
-        },
-        {
-          name: "Manoj Selvakumar",
-          role: "Web developer",
-          avatar: "",
-        },
-      ],
-      aboutSectionCards: [
-        {
-          title: 'About',
-          text: 'Video Genesis is a video streaming platform that demonstrates  super-resolution. Users can upscale their videos from low to high resolution.'
-        },
-        {
-          title: 'How it works',
-          text: 'Uploaded videos can be processed through a machine learning model called a Super Resolution Generative Adversarial Network (SRGAN). The model is trained by predicting high-resolution images from their low-resolution counterparts.'
-        },
-        {
-          title: 'Features',
-          text: "Users can upload videos, stream their own or other's content, create playlists, process videos with super-resolution, interact with videos, and more!"
-        }
-      ]
-    };
+    return {};
   },
 };
 </script>
