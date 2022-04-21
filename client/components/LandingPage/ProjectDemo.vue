25bc60471ba910b562a183c2b49fab10213d7a46
ritikb007 <94009884+ritikb007@users.noreply.github.com>
2022-04-20 22:02:44 -0700

    demo player settings

--- client/components/LandingPage/ProjectDemo.vue
+++ client/components/LandingPage/ProjectDemo.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="project-demo">
-    <v-row>
-      <v-col cols="12" class="text-center mt-16 white--text">
-        <h4 class="text-4xl">Project Demo</h4>
-        <video class="mx-auto mt-8" controls>
-          <source type="video/mp4" src="@/assets/videos/project_demo.mp4" />
-        </video>
-      </v-col>
-    </v-row>
+    <v-container>
+      <v-row>
+        <v-col cols="12" class="text-center mt-16 white--text">
+          <h4 class="text-4xl">Project Demo</h4>
+          <video class="mx-auto mt-8" controls>
+            <source type="video/mp4" src="@/assets/videos/FinalVersion.mp4" />
+          </video>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <style scoped>
 .project-demo {
   background: #240e1e;
-  height: 70vh;
+  height: 100vh;
 }
 </style>
