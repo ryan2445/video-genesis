a3790097ca9a3b244455b498afb912772410008b
heribertom127 <heribertom1270@gmail.com>
2022-04-21 00:36:26 -0700

    added a custom hover to button

--- client/components/UploadVideoButton.vue
+++ client/components/UploadVideoButton.vue
@@ -7,20 +7,23 @@
     :close-on-content-click="false"
   >
     <template v-slot:activator="{ on, attrs }">
-      <v-btn
-        style="right: 0%; margin-right: 8px"
-        absolute
-        v-bind="attrs"
-        v-on="on"
-        dense
-        height="30"
-        color="#A13440"
+      <v-hover v-slot="{ hover }">
+        <v-btn
+          style="right: 0%; margin-right: 8px"
+          absolute
+          v-bind="attrs"
+          v-on="on"
+          dense
+          height="30"
+          color="#A13440"
+          :style="{ 'background-color': hover ? '#c64050' : '#A13440' }"
+        >
+          <div class="text-white">
+            <v-icon class="mr-2"> mdi-cloud-upload </v-icon>
+            Upload Video
+          </div>
+        </v-btn></v-hover
       >
-        <div class="text-white">
-          <v-icon class="mr-2"> mdi-cloud-upload </v-icon>
-          Upload Video
-        </div>
-      </v-btn>
     </template>
     <upload-video-form @close="showUploadVideoForm = false" />
   </v-menu>
