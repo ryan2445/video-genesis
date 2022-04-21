38b46be14e05d69b3ebdf2dd9368a2cc12335db8
Manoj166 <mselvakumar@nevada.unr.edu>
2022-04-20 17:57:53 -0700

    Resources, need to fix some stuff

---
+++ client/components/LandingPage/Resources.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="resources pb-11">
+        <div class="w-11/12 mx-auto">
+            <v-row>
+                <v-col cols="12" class="mt-16 white--text">
+                    <h4 class="text-4xl">Resources</h4>
+                    <hr class="mt-7">
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="12" class="mt-8 white--text">
+                    <div class="sub-accordions">
+                        <h4 class="text-2xl">Vue.js Framework</h4>
+                        <h6 class="text-sm">A progressive frontend Javascript framework for constructing user interfaces. It 
+                            is <br>commonly used to build Single-Page Applications. It provides developers with <br>tooling and 
+                            libraries to create dynamic web applications.</h6>
+                    </div>
+                    <hr class="mt-7">
+                </v-col>
+                 <v-col cols="12" class="mt-8 white--text">
+                    <div class="sub-accordions">
+                        <h4 class="text-2xl">Vue.js Framework</h4>
+                        <h6 class="text-sm">A progressive frontend Javascript framework for constructing user interfaces. It 
+                            is <br>commonly used to build Single-Page Applications. It provides developers with <br>tooling and 
+                            libraries to create dynamic web applications.</h6>
+                    </div>
+                    <hr class="mt-7">
+                </v-col>
+                 <v-col cols="12" class="mt-8 white--text">
+                    <div class="sub-accordions">
+                        <h4 class="text-2xl">Vue.js Framework</h4>
+                        <h6 class="text-sm">A progressive frontend Javascript framework for constructing user interfaces. It 
+                            is <br>commonly used to build Single-Page Applications. It provides developers with <br>tooling and 
+                            libraries to create dynamic web applications.</h6>
+                    </div>
+                    <hr class="mt-7">
+                </v-col>
+                 <v-col cols="12" class="mt-8 white--text">
+                    <div class="sub-accordions">
+                        <h4 class="text-2xl">Vue.js Framework</h4>
+                        <h6 class="text-sm">A progressive frontend Javascript framework for constructing user interfaces. It 
+                            is <br>commonly used to build Single-Page Applications. It provides developers with <br>tooling and 
+                            libraries to create dynamic web applications.</h6>
+                    </div>
+                    <hr class="mt-7">
+                </v-col>
+                 <v-col cols="12" class="mt-8 white--text">
+                    <div class="sub-accordions">
+                        <h4 class="text-2xl">Vue.js Framework</h4>
+                        <h6 class="text-sm">A progressive frontend Javascript framework for constructing user interfaces. It 
+                            is <br>commonly used to build Single-Page Applications. It provides developers with <br>tooling and 
+                            libraries to create dynamic web applications.</h6>
+                    </div>
+                    <hr class="mt-7">
+                </v-col>
+            </v-row>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .resources {
+        background: #240E1E;
+    }
+
+    .sub-accordions:hover {
+        color: #F8FFCA;
+    }
+</style>
