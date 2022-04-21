631837cdb851d027d311ed8f2b039ff4627a6358
heribertom127 <heribertom1270@gmail.com>
2022-04-20 23:22:14 -0700

    changed playlist layout a tiny bit

--- client/components/PlaylistPageInfoColumn.vue
+++ client/components/PlaylistPageInfoColumn.vue
@@ -1,73 +1,75 @@
 <template>
-  <div
-    class="playlist-info-container h-screen bg-gray-200 flex"
-  >
+  <div class="playlist-info-container height-side bg-gray-200 flex">
     <div class="flex flex-col w-full pt-4 px-2">
       <div class="playlist-info-thumbnail-container mb-4">
         <playlist-thumbnail :playlist="playlist" :to="playAllLink" />
       </div>
       <div class="playlist-info-title-container">
-        <div class="flex flex-row">
-          <h1 v-if="!playlistTitleEdit" class="text-lg font-bold">
-            {{ playlist.playlistTitle }} 
+        <div class="playlist-info-user-container mb-2">
+          <profile-picture-and-username :user="playlist.user" />
+        </div>
+        <div class="playlist-info-data-container">
+          <div class="flex flex-row">
+            <p class="text-xs">
+              {{ playlistsDataString }}
+            </p>
+          </div>
+        </div>
+        <span class="font-bold">Playlist Title:</span>
+        <div class="flex flex-row my-1">
+          <h1 v-if="!playlistTitleEdit" class="text-lg">
+            {{ playlist.playlistTitle }}
           </h1>
           <v-text-field
             label="update playlist title"
             v-else-if="playlistTitleEdit"
             v-model="playlistTitleCopy"
             hide-details
-            class="pa-0 ma-0"
-            color="orange"
+            class="pa-0 ma-0 mt-4"
+            color="#A13440"
             @keydown.enter.prevent="handleTitleEdit"
           />
-        <div class="flex flex-1 justify-end">
-          <v-btn icon @click="handleTitleEdit">
-            <v-icon small>
-              {{ !playlistTitleEdit ? 'icon-pencil-outline' : 'mdi-content-save' }}
-            </v-icon>
-          </v-btn>
-        </div>
-        </div>
-      </div>
-      <div class="playlist-info-user-container">
-        <profile-picture-and-username :user="playlist.user" />
-      </div>
-      <div class="playlist-info-data-container">
-        <div class="flex flex-row mt-3">
-          <p class="text-xs">
-            {{ playlistsDataString }}
-          </p>
+          <div class="flex flex-1 justify-end">
+            <v-btn icon @click="handleTitleEdit">
+              <v-icon small>
+                {{
+                  !playlistTitleEdit
+                    ? "icon-pencil-outline"
+                    : "mdi-content-save"
+                }}
+              </v-icon>
+            </v-btn>
+          </div>
         </div>
       </div>
-      <v-divider></v-divider>
+
+      <span class="font-bold">Playlist Description:</span>
       <div class="playlist-info-description-container">
         <div class="flex flex-row flex-1">
           <div v-if="!playlistDescriptionEdit">
             <p v-if="playlist.description">
               {{ playlist.description }}
             </p>
-            <p v-else class="text-xs text-gray-700 ">
-              ...
-            </p>
+            <p v-else class="text-xs text-gray-700">...</p>
           </div>
           <v-textarea
             v-else
             label="update description"
             v-model="playlistDescriptionCopy"
-            color="orange"
+            color="#A13440"
             class="pa-0 mt-4 w-full"
             hide-details
             @keydown.enter.prevent="handleDescriptionEdit"
           >
-
           </v-textarea>
           <div class="flex flex-1 justify-end">
-            <v-btn
-              icon class="pa-0 ma-0"
-              @click="handleDescriptionEdit"
-            >
+            <v-btn icon class="pa-0 ma-0" @click="handleDescriptionEdit">
               <v-icon small>
-                {{ !playlistDescriptionEdit ? 'icon-pencil-outline' : 'mdi-content-save' }}
+                {{
+                  !playlistDescriptionEdit
+                    ? "icon-pencil-outline"
+                    : "mdi-content-save"
+                }}
               </v-icon>
             </v-btn>
           </div>
@@ -83,19 +85,19 @@ export default {
   props: {
     playlist: {
       type: Object,
-      required: true
-    }
+      required: true,
+    },
   },
   data() {
     return {
       playlistTitleEdit: false,
-      playlistTitleCopy: '',
+      playlistTitleCopy: "",
 
       playlistDescriptionEdit: false,
-      playlistDescriptionCopy: '',
+      playlistDescriptionCopy: "",
 
-      updating: false
-    }
+      updating: false,
+    };
   },
   mounted() {
     this.init();
@@ -108,47 +110,64 @@ export default {
     handleTitleEdit() {
       this.playlistTitleEdit = !this.playlistTitleEdit;
 
-      if (!this.playlistTitleEdit && this.playlistTitleCopy !== this.playlist.playlistTitle) {
-        this.updatePlaylist({playlistTitle: this.playlistTitleCopy})
+      if (
+        !this.playlistTitleEdit &&
+        this.playlistTitleCopy !== this.playlist.playlistTitle
+      ) {
+        this.updatePlaylist({ playlistTitle: this.playlistTitleCopy });
       }
     },
     handleDescriptionEdit() {
       this.playlistDescriptionEdit = !this.playlistDescriptionEdit;
 
-      if (!this.playlistDescriptionEdit && this.playlistDescriptionCopy !== this.playlist.description) {
-        this.updatePlaylist({description: this.playlistDescriptionCopy})
+      if (
+        !this.playlistDescriptionEdit &&
+        this.playlistDescriptionCopy !== this.playlist.description
+      ) {
+        this.updatePlaylist({ description: this.playlistDescriptionCopy });
       }
     },
     async updatePlaylist(params) {
       const payload = {
         ...this.playlist,
-        ...params
-      }
+        ...params,
+      };
 
-      const response = await this.$store.dispatch('playlists/playlistsPut', payload)
+      const response = await this.$store.dispatch(
+        "playlists/playlistsPut",
+        payload
+      );
 
-      this.$store.commit('playlists/playlistUpdate', payload)
+      this.$store.commit("playlists/playlistUpdate", payload);
 
       return response;
-    }
+    },
   },
   computed: {
     playlistsDataString() {
       const videosCount = this.playlist.videos.length;
 
-      return `${videosCount} video${videosCount > 1 ? 's' : ''}`
+      return `${videosCount} video${videosCount > 1 ? "s" : ""}`;
     },
     playAllLink() {
-      if (!this.playlist || !this.playlist.videos || this.playlist.videos.length <=0 ) return
-      return `/videos/pk=${this.playlist.videos[0].videoPK}&sk=${this.playlist.videos[0].videoSK}&listPK=${this.playlist.pk}&listSK=${this.playlist.sk}&index=0`
-    }
-  }
-}
+      if (
+        !this.playlist ||
+        !this.playlist.videos ||
+        this.playlist.videos.length <= 0
+      )
+        return;
+      return `/videos/pk=${this.playlist.videos[0].videoPK}&sk=${this.playlist.videos[0].videoSK}&listPK=${this.playlist.pk}&listSK=${this.playlist.sk}&index=0`;
+    },
+  },
+};
 </script>
 
 <style scoped>
 .playlist-info-container {
   max-width: 360px;
   width: 100%;
+}
+.height-side {
+  height: calc(100vh - 76px);
 }
 </style>
--- client/components/PlaylistTable.vue
+++ client/components/PlaylistTable.vue
@@ -1,20 +1,14 @@
 <template>
-  <div className="playlist-table-container w-full h-screen flex flex-1 flex-grow bg-white"
-    style="width: 100%; background: white; height: 100%;"
+  <div
+    className="playlist-table-container w-full h-screen flex flex-1 flex-grow bg-white"
+    style="width: 100%; background: white; height: 100%"
   >
-    <div
-      class="w-full"
-    >
-      <v-list
-        flat
-        width="100%"
-      >
-        <v-subheader>
-          Videos
-        </v-subheader>
+    <div class="w-full">
+      <v-list flat width="100%">
+        <v-subheader> Videos </v-subheader>
         <v-divider />
         <v-list-item
-          v-for="(playlistItem) of playlist.videos"
+          v-for="playlistItem of playlist.videos"
           :key="`${playlistItem.sk}`"
         >
           <v-list-item-icon>
@@ -32,15 +26,17 @@
           </v-list-item-icon>
           <v-list-item-content two-line>
             <v-list-item-title>
-              <NuxtLink :to="`videos/pk=${playlistItem.video.pk}&sk=${playlistItem.video.sk}`">
+              <NuxtLink
+                :to="`videos/pk=${playlistItem.video.pk}&sk=${playlistItem.video.sk}`"
+              >
                 <div class="text-gray-800 cardTitle">
-                    {{ playlistItem.video.videoTitle }}
+                  {{ playlistItem.video.videoTitle }}
                 </div>
               </NuxtLink>
             </v-list-item-title>
             <v-list-item-subtitle>
               <NuxtLink :to="`users/username=${playlist.user.pk.substr(3)}`">
-                <div>
+                <div class="text-color">
                   {{ playlist.user | userDisplayName }}
                 </div>
               </NuxtLink>
@@ -54,9 +50,7 @@
               :loading="deleting"
               :disabled="deleting"
             >
-              <v-icon>
-                icon-delete
-              </v-icon>
+              <v-icon> icon-delete </v-icon>
             </v-btn>
           </v-list-item-action>
         </v-list-item>
@@ -71,38 +65,47 @@ export default {
   props: {
     playlist: {
       required: true,
-      type: Object
-    }
+      type: Object,
+    },
   },
   data() {
     return {
-      bucket_url: "https://genesis2vod-staging-output-q1h5l756.s3.us-west-2.amazonaws.com",
-      deleting: false
-    }
+      bucket_url:
+        "https://genesis2vod-staging-output-q1h5l756.s3.us-west-2.amazonaws.com",
+      deleting: false,
+    };
   },
   methods: {
     onThumbnailClick(video) {
-      this.$router.push(
-        `/videos/pk=${video.pk}&sk=${video.sk}`
-      )
+      this.$router.push(`/videos/pk=${video.pk}&sk=${video.sk}`);
     },
     async onDelete(video) {
-      this.deleting = true
+      this.deleting = true;
 
       try {
         const params = {
           videos: [video],
-          sk: this.playlist.sk
-        }
-        const response = await this.$store.dispatch('playlists/playlistDeleteVideos', params)
+          sk: this.playlist.sk,
+        };
+        const response = await this.$store.dispatch(
+          "playlists/playlistDeleteVideos",
+          params
+        );
+      } catch (e) {
+        console.error("Error in deleting video from playlist");
+      } finally {
+        this.deleting = false;
       }
-      catch(e) {
-        console.error('Error in deleting video from playlist')
-      }
-      finally {
-        this.deleting = false
-      }
-    } 
-  }
-}
+    },
+  },
+};
 </script>
+
+<style scoped>
+.text-color {
+  color: #a13440;
+}
+.text-color:hover {
+  color: #ba5d67;
+}
+</style>
