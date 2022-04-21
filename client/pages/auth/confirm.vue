6dc4ba97ef95e152fec4f7161a70daefd3ef1b49
heribertom127 <heribertom1270@gmail.com>
2022-04-20 20:56:54 -0700

    fixed the confirm page

--- client/pages/auth/confirm.vue
+++ client/pages/auth/confirm.vue
@@ -1,29 +1,35 @@
 <template>
-    <div class="flex flex-col m-auto text-center" style="width: 360px">
-        <div v-if="user">
-            <h1>Confirm Email</h1>
-            <ConfirmAuthForm @confirmed="onConfirmed" />
-        </div>
-        <div v-else>
-            <h1>No user found</h1>
-        </div>
+  <div
+    class="flex flex-col m-auto h-screen place-content-center text-center bg-gray-200"
+  >
+    <div
+      class="p-7 place-self-center bg-white rounded-md"
+      v-if="user"
+      style="width: 360px"
+    >
+      <h1>Confirm Email</h1>
+      <ConfirmAuthForm @confirmed="onConfirmed" />
     </div>
+    <div v-else>
+      <h1>No user found</h1>
+    </div>
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 export default {
-    computed: {
-        ...mapGetters({
-            user: 'users/rootUser'
-        })
+  computed: {
+    ...mapGetters({
+      user: "users/rootUser",
+    }),
+  },
+  methods: {
+    onConfirmed() {
+      this.$router.push({
+        path: "/auth/sign-in",
+      });
     },
-    methods: {
-        onConfirmed() {
-            this.$router.push({
-                path: '/auth/sign-in'
-            })
-        }
-    }
-}
+  },
+};
 </script>
