91e4929085c134c96633cf84284cb9c9d2acb554
ritikb007 <94009884+ritikb007@users.noreply.github.com>
2022-04-20 18:21:28 -0700

    small fix

--- client/components/LandingPage/Team.vue
+++ client/components/LandingPage/Team.vue
@@ -1,56 +1,60 @@
 <template>
-    <div class="team-row h-screen">
-        <v-row>
-            <v-col cols="12" class="text-center mt-16 white--text">
-                <h4 class="text-4xl">Meet Team 24</h4>
-                <p class="mt-2">We are Team 24, and our instructors are Dr.David Feil-Seifer and Devrin Lee. Our <br>
-                    external advisor is Dr.Rui Wu. We are excited for you to check out our application. <br>
-                    We worked hard to make our application functional and beautiful. Enjoy!
-                </p>
-            </v-col>
-        </v-row>
-        <v-row class="white--text px-17 text-center">
-            <v-col cols="4">
-                <div class="rounded-full">
-                    <img class="mx-auto" src="@/assets/images/img1.png" />
-                </div>
-                <h4>Mark Minoff</h4>
-                <h6>CS Student</h6>
-            </v-col>
-            <v-col cols="4">
-                <div class="rounded-full">
-                    <img class="mx-auto" src="@/assets/images/img2.png" />
-                </div>
-                <h4>Mark Minoff</h4>
-                <h6>CS Student</h6>
-            </v-col>
-            <v-col cols="4">
-                <div class="rounded-full">
-                    <img class="mx-auto" src="@/assets/images/img3.png" />
-                </div>
-                <h4>Mark Minoff</h4>
-                <h6>CS Student</h6>
-            </v-col>
-            <v-col cols="4" class="mt-4">
-                <div class="rounded-full">
-                    <img class="mx-auto" src="@/assets/images/img4.png" />
-                </div>
-                <h4>Mark Minoff</h4>
-                <h6>CS Student</h6>
-            </v-col>
-            <v-col cols="4" class="mt-4">
-                <div class="rounded-full">
-                    <img class="mx-auto" src="@/assets/images/img5.png" />
-                </div>
-                <h4>Mark Minoff</h4>
-                <h6>CS Student</h6>
-            </v-col>
-        </v-row>
-    </div>
+  <div class="team-row h-screen">
+    <v-row>
+      <v-col cols="12" class="text-center mt-16 white--text">
+        <h4 class="text-4xl">Meet Team 24</h4>
+        <p class="mt-2">
+          We are Team 24, and our instructors are Dr.David Feil-Seifer and
+          Devrin Lee. Our <br />
+          external advisor is Dr.Rui Wu. We are excited for you to check out our
+          application. <br />
+          We worked hard to make our application functional and beautiful.
+          Enjoy!
+        </p>
+      </v-col>
+    </v-row>
+    <v-row class="white--text px-17 text-center">
+      <v-col cols="4">
+        <div class="rounded-full">
+          <img class="mx-auto" src="@/assets/images/img1.png" />
+        </div>
+        <h4>Mark Minoff</h4>
+        <h6>CS Student</h6>
+      </v-col>
+      <v-col cols="4">
+        <div class="rounded-full">
+          <img class="mx-auto" src="@/assets/images/img2.png" />
+        </div>
+        <h4>Ryan Hoffman</h4>
+        <h6>CS Student</h6>
+      </v-col>
+      <v-col cols="4">
+        <div class="rounded-full">
+          <img class="mx-auto" src="@/assets/images/img3.png" />
+        </div>
+        <h4>Ritik Bhardwaj</h4>
+        <h6>CS Student</h6>
+      </v-col>
+      <v-col cols="4" class="mt-4">
+        <div class="rounded-full">
+          <img class="mx-auto" src="@/assets/images/img4.png" />
+        </div>
+        <h4>Heriberto Martinez</h4>
+        <h6>CS Student</h6>
+      </v-col>
+      <v-col cols="4" class="mt-4">
+        <div class="rounded-full">
+          <img class="mx-auto" src="@/assets/images/img5.png" />
+        </div>
+        <h4>Manoj Selvakumar</h4>
+        <h6>CS Student</h6>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <style scoped>
-    .team-row {
-        background: #2E1226;
-    }
+.team-row {
+  background: #2e1226;
+}
 </style>
